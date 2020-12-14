3352724714aa37142ab36faacf81f120aa24f150
Wayne Jones <wayne.arfon.jones@googlemail.com>
2020-12-14 15:13:36 +0000

    Initial commit, basic project

---
+++ src/main/java/io/github/jonesthesoftware/yealink/phonebook/PhoneBookApplication.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2020 Wayne Jones
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+package io.github.jonesthesoftware.yealink.phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( PhoneBookApplication.class, args );
+	}
+	
+}
